c48f9e647476316b70c52904aba63d6684f84bb8
Dmitry Eshman <bakko7821@gmail.com>
2026-02-25 22:51:36 +0300

    install start UI dependencies
    -install vite (react + ts)
    -install tailwind

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-red-300 items-center justify-center flex">
+      <h1 className="text-2xl font-bold bg-blue-300">Проверка tailwind</h1>
+    </div>
+  );
+}
