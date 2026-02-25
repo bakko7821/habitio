46ddce465a38354a9e0f3fb41e1836c134f21443
Dmitry Eshman <bakko7821@gmail.com>
2026-02-25 23:55:11 +0300

    add roboto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
     <div className="w-screen h-screen bg-red-300 items-center justify-center flex">
-      <h1 className="text-2xl font-bold bg-blue-300">Проверка tailwind</h1>
+      <h1 className="text-2xl font-bold bg-blue-300 font-roboto">
+        Проверка tailwind
+      </h1>
     </div>
   );
 }
