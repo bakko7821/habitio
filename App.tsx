2730f9b9d11f1dbc093cdafe71ec73a62cd20941
Dmitry Eshman <bakko7821@gmail.com>
2026-03-17 23:40:24 +0300

    fix: app.json

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { View } from "react-native";
 export default function App() {
   return (
     <Provider store={store}>
-      <View></View>
+      <View className="w-screen h-screen flex-1 bg-red-600"></View>
     </Provider>
   );
 }
